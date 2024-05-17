e6be8fc7a3ee2c6ebb1a78f21bb3e6ddfb0b8bf9
mauriciorendonmo <mauricio.rendonmo@gmail.com>
2024-05-17 13:39:09 -0500

    cambio 1 en main

--- src/PartsUnlimited.Models/Raincheck.cs
+++ src/PartsUnlimited.Models/Raincheck.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+// cambio en 1 en main
 namespace PartsUnlimited.Models
 {
     public class Raincheck
